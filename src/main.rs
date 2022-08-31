e741620c0b9657109953551800832836c8ed8f77
Mark Christian <m@rkchristian.ca>
2022-08-31 16:55:44 -0700

    Starting to parse header

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::{Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,10 +22,80 @@ fn main() {
     }
 }
 
-fn handle_command(filename: &String, command: &String, params: &[String]) {
-    println!("Filename: {}, Command: {}, Params: {:?}", filename, command, params)
+fn handle_command(filename: &String, command: &str, params: &[String]) {
+    println!("Filename: {}, Command: {}, Params: {:?}", filename, command, params);
+
+    let wad = Wad::open(filename);
+
+    match command {
+        "list-maps" => list_maps(&wad),
+        "info" => show_info(&wad),
+        _ => {
+            println!("Sorry, I don't know how to {}", command);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn list_maps(_wad: &Wad) {
+}
+
+fn show_info(_wad: &Wad) {
 }
 
 fn print_usage_and_exit(executable: &String) {
     println!("usage: {} /path/to/a/doom.wad command", executable);
+}
+
+enum LumpType {
+    Blockmap,
+    LineDefs,
+    Nodes,
+    Reject,
+    Scripts,
+    Sectors,
+    Segs,
+    SideDefs,
+    SSectors,
+    Things,
+    Vertexes,
+}
+
+#[derive(Debug)]
+enum WadType {
+    IWAD,
+    PWAD
+}
+
+struct Wad {
+    wad_type: WadType
+}
+
+impl Wad {
+    fn open(filename: &str) -> Wad {
+        let mut f = match File::open(filename) {
+            Ok(file) => file,
+            Err(error) => panic!("Failed to open {}: {}", filename, error),
+        };
+
+        let mut header_buf = [0; 12];
+        let _ = f.read_exact(&mut header_buf);
+        println!("Header {:?}", header_buf);
+
+        let v = header_buf[0..4].to_vec();
+        let wad_type = match String::from_utf8(v) {
+            Ok(str) if str.eq("IWAD") => WadType::IWAD,
+            Ok(str) if str.eq("PWAD") => WadType::PWAD,
+            _ => panic!("Invalid WAD; expected signature {:?} to be 'IWAD' ({:?}) or 'PWAD' ({:?})",
+                header_buf[0..4].to_vec(),
+                String::from("IWAD").as_bytes(),
+                String::from("PWAD").as_bytes()
+            )
+        };
+        println!("wad_type {:?}", wad_type);
+
+        Wad {
+            wad_type: WadType::IWAD
+        }
+    }
 }
