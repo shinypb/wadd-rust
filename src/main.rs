3209507e35c52e0176e87fcbe74468eee25049f4
Mark Christian <m@rkchristian.ca>
2022-09-16 14:05:48 -0700

    Make the background dark rather than light

--- src/main.rs
+++ src/main.rs
@@ -254,10 +254,10 @@ fn extract_map(wad: &Wad, map_name: &str) {
     <meta charset="utf-8">
     <style>
         html {{
-            background: white;
+            background: black;
             color: white;
             /* https://stackoverflow.com/posts/35362074/revisions */
-            background-image: linear-gradient(45deg, #ccc 25%, transparent 25%), linear-gradient(-45deg, #ccc 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #ccc 75%), linear-gradient(-45deg, transparent 75%, #ccc 75%);
+            background-image: linear-gradient(45deg, #333 25%, transparent 25%), linear-gradient(-45deg, #333 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #333 75%), linear-gradient(-45deg, transparent 75%, #333 75%);
             background-size: 20px 20px;
             background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
         }}
